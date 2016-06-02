f34da94e005aa19dd8296b8c86b6d3512e5410a0
Karthik Duddu <kar.meher95@gmail.com>
2016-06-02 20:31:59 +0200

    Trained forest

--- CIFAR_RF/process.py
+++ CIFAR_RF/process.py
@@ -1,16 +1,53 @@
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import cPickle
+import os
 
 DATA_PATH = "data/"
 FILE_PREFIX = "data_batch"
+META_FILE = "batches.meta"
+label_names = []
 
-def unpack_data(data_path, file_prefix):
-	data = np.array()
-	for fname in os.listdirs(data_path):
+def show_image(data):
+	tmp = data.reshape(3, 32, 32).transpose(1, 2, 0)
+	plt.imshow(tmp)
+	plt.show()
+
+def get_label_names(fname):
+	with open(fname, 'rb') as f:
+		ln = cPickle.load(f)
+		return ln['label_names']
+
+def unpack_data(data_path, file_prefix):	
+	global label_names
+	count, data, labels = 0, np.empty(shape=(50000, 3072)), np.empty(shape=(50000))
+	for fname in os.listdir(data_path):
 		if file_prefix not in fname:
 			continue
-		with tmp as open(data_path + fname, 'rb'):
-			np.append( cPickle.load(tmp) )
+		tmp = open(data_path + fname, 'rb')
+		d = cPickle.load(tmp)
+		data[count*10000:(count+1)*10000, :] = d['data']
+		labels[count*10000:(count+1)*10000] = np.array(d['labels'])
+		# show_image(data[count*10000,:])
+		# print data[count*10000, :] == d['data'][0,:]
+		count += 1
+		print "Processing batch",count
+		# print d['data'].shape
+		# for k in d.keys():
+		# 	print k,type(d[k])
+		# print label_names[ d['labels'][1] ]
+		# show_image(d['data'][0,:])
+		tmp.close()
+	print "Finished Processing"
+	return data, labels
 
-print unpack_data(DATA_PATH, FILE_PREFIX).shape
+label_names = get_label_names(DATA_PATH + META_FILE)
+train, labels = unpack_data(DATA_PATH,FILE_PREFIX)
+print train.shape, labels.shape
+print "Initializing random forest"
+rf = RandomForestClassifier(n_estimators=10, n_jobs=2)
+print "Fitting data"
+rf.fit(train, labels)
+print "Training complete"
+# show_image(train[0,:])
